3186deaf23a4e84e0cf4440016a7e09536de6523
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-05-20 17:19:25 +0100

    add fonts

--- core/src/main/kotlin/net/rolodophone/ludumdare46/button/ButtonText.kt
+++ core/src/main/kotlin/net/rolodophone/ludumdare46/button/ButtonText.kt
@@ -1,18 +1,21 @@
 package net.rolodophone.ludumdare46.button
 
-import android.graphics.Paint
-import android.graphics.RectF
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.math.Rectangle
 import net.rolodophone.ludumdare46.State
 import net.rolodophone.ludumdare46.canvas
 import net.rolodophone.ludumdare46.paint
-import net.rolodophone.ludumdare46.w
+import net.rolodophone.ludumdare46.util.*
 
-open class ButtonText(val text: String, val align: Paint.Align, state: State, dim: RectF, val textSize: Float = dim.height() - w(3), onClick: () -> Unit) : Button(state, dim, onClick) {
+open class ButtonText(val text: String, val align: Align, state: State, dim: Rectangle, val textSize: Float = dim.height - w(3), onClick: () -> Unit) : Button(state, dim, onClick) {
+
+    private val font = BitmapFont(Gdx.files.internal("font/noto-sans.fnt"), Gdx.files.internal("font/noto-sans.png"), false)
 
     private val x = when (align) {
-        Paint.Align.LEFT -> dim.left
-        Paint.Align.RIGHT -> dim.right
-        Paint.Align.CENTER -> (dim.left + dim.right) / 2
+        Align.LEFT -> dim.left
+        Align.RIGHT -> dim.right
+        Align.CENTER -> (dim.left + dim.right) / 2
     }
 
 
@@ -22,5 +25,12 @@ open class ButtonText(val text: String, val align: Paint.Align, state: State, di
         paint.textAlign = align
         paint.textSize = textSize
         canvas.drawText(text, x, dim.centerY() + textSize/2 - w(3), paint)
+
+        font.setColor(0f, 0f, 0f, 1f)
+        font.draw()
     }
+}
+
+enum class Align {
+    LEFT, RIGHT, CENTER
 }
